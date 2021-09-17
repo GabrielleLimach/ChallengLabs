fa9e51ddc0f683239b4b209e54deb7299349d9d3
Gabrielle Limach <gabrielle.lmm@gmail.com>
2021-09-17 10:38:35 -0300

    M - implementacao de config do banco

---
+++ src/main/java/com/challenge/labs/config/db/JPAConfig.java
@@ -0,0 +1,19 @@
+package com.challenge.labs.config.db;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+public class JPAConfig {
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+        return transactionManager;
+    }
+}
