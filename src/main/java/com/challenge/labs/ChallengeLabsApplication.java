f5afd9311b64106b7165fea38701b54040a3fc77
Gabrielle Limach <gabrielle.lmm@gmail.com>
2021-09-17 01:07:37 -0300

    D - Inicializacao do projeto do desafio tecnico de uma plataforma de comunicacao

---
+++ src/main/java/com/challenge/labs/ChallengeLabsApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/labs/ChallengeLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
